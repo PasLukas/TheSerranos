ea2e37ea52e7367cd83c9f875bbcd7719dcbc4f5
Pascal_Lukas <70589803+PasLukas@users.noreply.github.com>
2025-05-29 20:45:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const correctPassword = "APEN_IN_NL";
+let player;
 
 function checkPassword() {
   const input = document.getElementById("passwordInput").value;
@@ -8,7 +9,9 @@ function checkPassword() {
     document.getElementById("passwordSection").style.display = "none";
     document.getElementById("giftSection").classList.remove("hidden");
     document.getElementById("musicPlayer").classList.remove("hidden");
+
     launchBalloons();
+    playMusic();
   } else {
     error.textContent = "Falsches Passwort. Versuche es erneut!";
   }
@@ -23,5 +26,24 @@ function launchBalloons() {
     balloon.style.left = Math.random() * 90 + "%";
     container.appendChild(balloon);
     setTimeout(() => container.removeChild(balloon), 4000);
+  }
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('ytplayer', {
+    height: '0',
+    width: '0',
+    videoId: 'Yc-7IQqcqeM',
+    playerVars: {
+      'autoplay': 0,
+      'loop': 1,
+      'playlist': 'Yc-7IQqcqeM'
+    }
+  });
+}
+
+function playMusic() {
+  if (player && player.playVideo) {
+    player.playVideo();
   }
 }
